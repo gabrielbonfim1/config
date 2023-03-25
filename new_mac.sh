0be52c7f856682f09050bc79995c8a2883cd2202
Gabriel <gabriel.zip@hotmail.com>
2023-03-25 13:32:17 -0300

    first commit

---
+++ new_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Preparing a new MAC for development
+# Script Created by gabrielbonfim1.medium.com
+
+#INSTALL HOMEBREW
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+#INSTALL iTerm2
+brew install --cask iterm2
+
+#INSTALL ZSH
+brew install zsh
+
+#Define HomeBrew's ZSH as Default Shell
+chsh -s /opt/homebrew/bin/zsh
+
+#Install OH-My-ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Install PowerLevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
